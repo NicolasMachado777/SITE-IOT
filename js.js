69d93ab264bd20c109d5b77af443ccda4b5af016
Matheus <matheus.mariano28@etec.sp.gov.br>
2023-06-05 21:42:39 -0300

    sistema do quiz

--- js.js
+++ js.js
@@ -31,22 +31,149 @@ function openbar() {
     
 }
 
-function respostacerta() {
-    let resposta = document.getElementById("nicolas");
-    var respostas = document.getElementsByClassName("resposta");
-    resposta.style.backgroundColor="green";
-    for (let i = 1; i < 4; i++) {
-        respostas[i].style.backgroundColor="red";
+/*__________________________________________________________________________________________*/
+
+const botaoIniciar = document.querySelector(".comecar-quiz")
+const perguntasContainer = document.querySelector(".perguntas-container")
+const respostasContainer = document.querySelector(".respostas-container")
+const questaoTexto = document.querySelector(".questao")
+const tudo = document.querySelector(".caixa")
+const proximabotao = document.querySelector(".proxima-questao")
+
+botaoIniciar.addEventListener("click", comecarjogo)
+proximabotao.addEventListener("click", mostrarProxPergunta)
+
+let questaoAtual = 0
+let totalacertos = 0
+
+function comecarjogo(){
+    botaoIniciar.classList.add("hide")
+    perguntasContainer.classList.remove("hide")
+    mostrarProxPergunta()
+}
+
+function mostrarProxPergunta(){
+    
+    resetarquiz()
+
+    if(questions.length === questaoAtual){
+        return terminarjogo()
     }
+
+    questaoTexto.textContent = questions[questaoAtual].question
+    questions[questaoAtual].answers.forEach(answer => {
+        const NovaQuestao = document.createElement("button")
+        NovaQuestao.classList.add("resposta", "botao")
+        NovaQuestao.textContent = answer.text
+        if(answer.correct){
+            NovaQuestao.dataset.correto = answer.correct
+        }
+        respostasContainer.appendChild(NovaQuestao)
+
+        NovaQuestao.addEventListener("click", questaoselecionada)
+    })
+}
+
+function resetarquiz(){
+    while(respostasContainer.firstChild){
+        respostasContainer.removeChild(respostasContainer.firstChild)
+    }
+
+    proximabotao.classList.add("hide")
 
 }
 
-function passarpergunta() {
-    let next = document.getElementById("next")
-    let questoes = document.getElementsByClassName("questoes")
-    for (let i = 0; i < 2; i++) {
-        questoes[i].style.display="none";
-        questoes[i+1].style.display="flex";
-        
+function questaoselecionada(event){
+    const questaoclicada = event.target
+
+    if(questaoclicada.dataset.correto){
+       /* tudo.classList.add("correto")*/
+        totalacertos++
+    }else{
+        /*tudo.classList.add("incorreto")*/
+
     }
+
+    document.querySelectorAll(".resposta").forEach(button =>{
+        if(button.dataset.correto){
+            button.classList.add("correto")
+
+        }else{
+            button.classList.add("incorreto")
+
+        }
+
+    })
+
+    proximabotao.classList.remove("hide")
+    questaoAtual++
 }
+
+function terminarjogo(){
+    const totalquestoes = questions.length
+    const performance = Math.floor(totalacertos * 100 / totalquestoes)
+
+    let mensagem = ""
+
+    switch (true) {
+        case performance >= 90:
+            mensagem = "Excelente :)"
+            break;
+        case performance >= 70:
+            mensagem = "Muito bom :)"
+            break;
+        case performance >= 50:
+            mensagem = "Bom :)"
+            break;
+        default:
+            mensagem = "Da pra melhorar :("
+            break;
+    }
+
+    perguntasContainer.innerHTML = ` <p class = "mensagem-final"> Você acertou ${totalacertos} de ${totalquestoes} questões!
+    <span>Resultado: ${mensagem}</span> </p> <button onclick= window.location.reload() class="botao"> Refazer teste </button>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions = [
+    {
+      question: "O joao é chato?",
+      answers: [
+        { text: "Não", correct: false },
+        { text: "Nunca", correct: false },
+        { text: "Sim", correct: true },
+        { text: "Jamais", correct: false }
+      ]
+    },
+    {
+      question: "Mais esperto da Etec?",
+      answers: [
+        { text: "Abner", correct: true },
+        { text: "Nicolas", correct: false },
+        { text: "Davi", correct: false },
+        { text: "Matheus", correct: false }
+      ]
+    },
+    {
+      question: 'Davi pega mulher casada?',
+      answers: [
+        { text: 'Sim', correct: true },
+        { text: 'Não', correct: false },
+        { text: 'Nunca', correct: false },
+        { text: "Jamais", correct: false }
+      ]
+    }
+  ]
